55f26e991297e63345697024c6c1fa00e8bd3825
CTY <42131250+CTYTC@users.noreply.github.com>
2022-05-07 11:53:41 +0800

    implement move

--- Rebellion/src/Turtle.java
+++ Rebellion/src/Turtle.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.Math;
+
 /**
  * Representation of a single actor (cop or agent) on the model board
  */
@@ -17,6 +21,62 @@ public abstract class Turtle {
      * Moves the actor to an empty space in vision range
      */
     public void move(Turtle[][] map){
-        // TODO
+        List<int[]> empty_patch = getEmptyPatch(map);
+
+        int rand = (int)(Math.random() * (empty_patch.size() - 1));
+        int[] new_coordinate = empty_patch.get(rand);
+
+        map[this.location_x][this.location_y] = null;
+        this.location_x = new_coordinate[0];
+        this.location_y = new_coordinate[1];
+        map[this.location_x][this.location_y] = this;
+
+    }
+
+    /**
+     *
+     * @param width width of the map
+     * @param height height of the map
+     * @return A list of coordinate within the vision
+     */
+    public List<int[]> getVision(int width, int height){
+        int x_range_low = this.location_x - vision;
+        int x_range_high = this.location_x + vision;
+        int y_range_low = this.location_y - vision;
+        int y_range_high = this.location_y + vision;
+        List<int[]> vision = new ArrayList<>();
+
+        for(int i = x_range_low; i <= x_range_high; i++){
+            for(int j = y_range_low; j <= y_range_high; j++){
+                int x = i, y = j;
+
+                if(x < 0) x += width;
+                else if (x > width - 1) x -= width;
+
+                if(y < 0) y += height;
+                else if (y > height - 1) y -= height;
+
+                vision.add(new int[] {x, y});
+            }
+        }
+
+        return vision;
+    }
+
+    /**
+     *
+     * @param turtleMap current map
+     * @return a list of coordinate within the vision and is unoccupied
+     */
+    public List<int[]> getEmptyPatch(Turtle[][] turtleMap){
+        List<int[]> vision = getVision(turtleMap[0].length, turtleMap.length);
+        List<int[]> empty_patch = new ArrayList<>();
+
+        for(int[] coordinate: vision){
+            if (turtleMap[coordinate[0]][coordinate[1]] == null){
+                empty_patch.add(coordinate);
+            }
+        }
+        return empty_patch;
     }
 }
